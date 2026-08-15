e2383619ca63a924387b8a28de943c42a49fb19b
Rithvicnallan <rithvicnallan@gmail.com>
2026-08-15 20:26:53 +0530

    Added input adn two different timers

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
 import time
 
-countdown = 5
 
-while countdown > 0:
+def timer(countdown):
+    while countdown > 0:
 
-    minutes = countdown//60
-    seconds = countdown%60
-    print(f'{minutes:02}:{seconds:02}\r',end="")
-    time.sleep(1)
-    countdown -= 1
+        minutes = countdown//60
+        seconds = countdown%60
+        print(f'{minutes:02}:{seconds:02}\r',end="", flush=True)
+        time.sleep(1)
+        countdown -= 1
+
+
+work_timer = input("Please input how long you would like to work for: ")
+break_timer = input("Please input how long you would like to have a break for: ")
+
+timer(int(work_timer))
+print("              \r", end="")
+print("Done, starting break now")
+
+timer(int(break_timer))
 
 print("              \r", end="")
-print("Do")
+print("Done")
